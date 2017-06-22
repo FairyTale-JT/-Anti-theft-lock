377efd2be738a9b1a27fe245b43d71a0c96a8a2d
hujialiang <362832308@qq.com>
2017-06-22 17:05:50 +0800

    fdshjl

--- JgPushTest/app/src/main/java/com/example/hjl/jgpushtest/fragment/JsTjAdapter.java
+++ JgPushTest/app/src/main/java/com/example/hjl/jgpushtest/fragment/JsTjAdapter.java
@@ -1,6 +1,8 @@
 package com.example.hjl.jgpushtest.fragment;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +21,20 @@ import java.util.List;
 
 public class JsTjAdapter extends BaseAdapter {
     private List<FdSuo> list;
+    private int arg=-1;
 
     public JsTjAdapter() {
 
 
     }
-
+public void setArg(int arg1){
+//    this.arg=arg1;
+//    notifyDataSetChanged();
+    if(arg1 != arg){
+        arg = arg1;
+        notifyDataSetChanged();
+    }
+}
     public void setsetDateJsTjAdapter(List<FdSuo> list) {
         this.list = list;
         notifyDataSetChanged();
@@ -46,21 +56,51 @@ public class JsTjAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View convertView, ViewGroup viewGroup) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.jstj_list_item, null);
+    public View getView(int i, View view, ViewGroup viewGroup) {
         FdSuo fdSuo = (FdSuo) getItem(i);
-        TextView tv1 = (TextView) view.findViewById(R.id.jstj_tv1);
-        TextView tv2 = (TextView) view.findViewById(R.id.jstj_tv2);
-        TextView tv3 = (TextView) view.findViewById(R.id.jstj_tv3);
-        TextView tv4 = (TextView) view.findViewById(R.id.jstj_tv4);
-        TextView tv5 = (TextView) view.findViewById(R.id.jstj_tv5);
-        TextView tv6 = (TextView) view.findViewById(R.id.jstj_tv6);
-        tv1.setText(fdSuo.getSuo_sbBH());
-        tv2.setText(fdSuo.getSuo_haoma());
-//        tv3.setText(fdSuo.getSuo_sbBH());
+        Util util = null;
+        if (view == null) {
+            util = new Util();
+            view = LayoutInflater.
+                    from(viewGroup.getContext()).
+                    inflate(R.layout.jstj_list_item, null);
+            util.tv1 = (TextView) view.findViewById(R.id.jstj_tv1);
+            util.tv2 = (TextView) view.findViewById(R.id.jstj_tv2);
+            util.tv3 = (TextView) view.findViewById(R.id.jstj_tv3);
+            util.tv4 = (TextView) view.findViewById(R.id.jstj_tv4);
+            util.tv5 = (TextView) view.findViewById(R.id.jstj_tv5);
+            util.tv6 = (TextView) view.findViewById(R.id.jstj_tv6);
+            view.setTag(util);
+        }else {
+            util = (Util) view.getTag();
+        }
+        util.tv1.setText(fdSuo.getSuo_sbBH());
+        util.tv2.setText(fdSuo.getSuo_haoma());
+//        util.tv3.setText(fdSuo.getSuo_sbBH());
 //        tv4.setText(fdSuo.getSuo_sbBH());
-        tv5.setText(Integer.toString(i + 1));
-        tv6.setText(fdSuo.getSuo_ztBJ());
+        util.tv5.setText(Integer.toString(i + 1));
+        util.tv6.setText(fdSuo.getSuo_ztBJ());
+        if (arg==i) {
+            util.tv5.setTextColor(viewGroup.getContext().getResources().getColor(R.color.bt1_blue));
+            util.tv6.setTextColor(viewGroup.getContext().getResources().getColor(R.color.bt1_blue));
+            util.tv1.setTextColor(viewGroup.getContext().getResources().getColor(R.color.bt1_blue));
+            util.tv2.setTextColor(viewGroup.getContext().getResources().getColor(R.color.bt1_blue));
+
+            Log.e("TAG",arg+"");
+           arg=-1;
+            Log.e("TAG",arg+"");
+        }else {
+            util.tv5.setTextColor(viewGroup.getContext().getResources().getColor(R.color.tv_black));
+            util.tv6.setTextColor(viewGroup.getContext().getResources().getColor(R.color.tv_black));
+            util.tv1.setTextColor(viewGroup.getContext().getResources().getColor(R.color.tv_black));
+            util.tv2.setTextColor(viewGroup.getContext().getResources().getColor(R.color.tv_black));
+        }
         return view;
+    }
+   static class Util {
+
+        TextView tv1, tv2, tv3,tv4,tv5,tv6;
+
+
     }
 }
